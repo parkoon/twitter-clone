779475ea3345cb590a9af10d367804036a4288f7
parkoon <devparkoon@gmail.com>
2020-09-16 21:39:16 +0900

    Updates

---
+++ precache-manifest.b4629399eba2fcd346381080554ec127.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afecda34c6d91fecd55061d7c894754e",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "86e3ccb755de8c514026",
+    "url": "/twitter-clone/static/js/2.b51c7a7e.chunk.js"
+  },
+  {
+    "revision": "6524af321f8d3e987e59afab1834492e",
+    "url": "/twitter-clone/static/js/2.b51c7a7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c76147dc1f651145e5de",
+    "url": "/twitter-clone/static/js/main.c158bbf6.chunk.js"
+  },
+  {
+    "revision": "4ea1c8ca6c4e7890228d",
+    "url": "/twitter-clone/static/js/runtime-main.a74d6e56.js"
+  }
+]);
