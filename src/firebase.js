bf4e6673ea6ffa480521cd60ead3e41faf2a4f4a
parkoon <devparkoon@gmail.com>
2020-09-16 15:49:46 +0900

    feat: react + firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,4 @@
+import * as firebase from 'firebase'
+import firebaseConfig from './config/firebaseConfig'
+
+export default firebase.initializeApp(firebaseConfig)
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import firebase from './firebase'
+
+console.log(firebase)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
